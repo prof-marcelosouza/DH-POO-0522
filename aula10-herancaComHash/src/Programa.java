c72bab42134e25f8a918d4914aa89b730493288d
Marcelo Gonçalves de Souza <marcelo.souza@dnweb.com.br>
2022-06-07 18:58:33 -0300

    Aula10 - Exemplo de herança com equals e hashcode.

---
+++ aula10-herancaComHash/src/Programa.java
@@ -0,0 +1,41 @@
+import entities.Empregado;
+import entities.Vendedor;
+
+public class Programa {
+
+    public static void main(String[] args) {
+
+        Vendedor v1 = new Vendedor(1, "Marcia", "20111", 2200.0, 525.0);
+        v1.setComissao(3);
+        v1.setTotalVendas(25.0);
+
+        System.out.println("Vendedor 1 instanciado(a):");
+        System.out.println(v1);
+
+        System.out.println("Instância do primeiro empregado");
+        Empregado emp1 = new Empregado(2, "Patrick", "10100", 1500.0, 220.0);
+        System.out.println(emp1);
+
+        System.out.println("Instância do segundo empregado");
+        Empregado emp2 = new Empregado(2, "Patrick", "10100", 1500.0, 220.0);
+        System.out.println(emp2);
+
+        System.out.println("Instância do terceiro empregado");
+        Empregado emp3 = new Empregado(4, "Paulo", "10100", 1500.0, 220.0);
+        System.out.println(emp3);
+
+        System.out.println("");
+        System.out.println("");
+
+        // TESTANDO O OBJETO EMP1 COM O EMP2
+        if (emp1.equals(emp2)) {
+            System.out.println("Os conteúdos dos atributos SÃO IGUAIS.");
+        } else {
+            System.out.println("Os conteúdos dos atributos NÃO SÃO IGUAIS.");
+        }
+
+        System.out.println(emp1.hashCode());
+        System.out.println(emp2.hashCode());
+        System.out.println(emp3.hashCode());
+    }
+}
