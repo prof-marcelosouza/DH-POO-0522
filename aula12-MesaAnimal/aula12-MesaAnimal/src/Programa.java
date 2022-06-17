22eabcf104d998e788137acbc5d5c4aab7a5d2f5
Marcelo Gonçalves de Souza <mgoncalves@digitalhouse.com>
2022-06-17 06:12:55 -0300

    Aula12 - Interface - Mesa Animal.

--- aula12-MesaAnimal/aula12-MesaAnimal/src/Programa.java
+++ aula12-MesaAnimal/aula12-MesaAnimal/src/Programa.java
@@ -6,20 +6,20 @@ public class Programa {
 
     public static void main(String[] args) {
 
-        Cachorro c1 = new Cachorro(1, "Bob", 5);
-        System.out.println(c1);
-        c1.emitirSom();
-        c1.correr();
+        Cachorro cac1 = new Cachorro(1, "Bob", 5);
+        System.out.println(cac1);
+        cac1.emitirSom();
+        cac1.correr();
 
         Cavalo cav1 = new Cavalo(2, "Corcel", 6);
         System.out.println(cav1);
         cav1.emitirSom();
         cav1.correr();
 
-        Preguica p1 = new Preguica(3, "Flecha", 9);
-        System.out.println(p1);
-        p1.emitirSom();
-        p1.subirArvore();
+        Preguica pre1 = new Preguica(3, "Flecha", 9);
+        System.out.println(pre1);
+        pre1.emitirSom();
+        pre1.subirArvore();
     }
 
 }
